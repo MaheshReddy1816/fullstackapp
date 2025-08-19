7659799f5735413f32cef1a83a02b13974f90f0e
MaheshReddy1816 <maheshreddy1816@gmail.com>
2025-08-20 01:08:06 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://54.209.93.56/springapp1';
+const BASE_URL = 'http://54.209.93.56:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
